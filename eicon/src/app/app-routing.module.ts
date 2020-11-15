629570fc28494b211e1bd1fe9270d6afa4956c2b
Ivan Vidal <ivanvalmeida.123@gmail.com>
2020-11-15 12:40:29 -0300

    finalizado layout

--- eicon/src/app/app-routing.module.ts
+++ eicon/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
+  { path: 'details', component: DetailsComponent},
 ];
 
 @NgModule({
